027c714f2c0922a8397b6685a2f24b8c570bd8e2
UnexpiredBread <unexpiredbread@gmail.com>
2024-05-07 23:20:38 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: "https://unexpiredbread.github.io",
+  base: "dvn",
   image: {
     service: passthroughImageService(),
   },
